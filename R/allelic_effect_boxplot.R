9075866ee65816c1d5a0fab16ab3b1599d34818b
serbier <s.cruz@cgiar.org>
2025-04-07 22:49:15 -0500

    Add main page

--- R/allelic_effect_boxplot.R
+++ R/allelic_effect_boxplot.R
@@ -7,59 +7,52 @@ allelicEffectPlot_ui <- function(id){
       layout_column_wrap(
         value_box(
           title = "PEV",
-          value = textOutput(ns('pevValue')),
+          value = textOutput(ns("pevValue")),
         ),
         value_box(
           title = "LOD",
-          value = textOutput(ns('lodValue')),
+          value = textOutput(ns("lodValue")),
         ),
         value_box(
           title = "Favorable Allele",
-          value = textOutput(ns('faValue')),
+          value = textOutput(ns("faValue")),
         ),
         value_box(
           title = "Effect",
-          value = textOutput(ns('effValue')),
+          value = textOutput(ns("effValue")),
         ),
         value_box(
           title = "MAF",
-          value = textOutput(ns('mafValue')),
+          value = textOutput(ns("mafValue")),
         ),
       ),
-      plotlyOutput(ns('allelicEffectBoxplot'))
+      plotlyOutput(ns("allelicEffectBoxplot"))
     )
   )
   
 }
 
-allelicEffectPlot_server <- function(id, qtlData, gtCol = 'GT',
-                                     designation = 'Designation',
-                                     response = 'Response',
-                                     pev,
-                                     lod,
-                                     fa,
-                                     eff,
-                                     maf
-                                     ){
-  
+allelicEffectPlot_server <- function(id, qtlData, gtCol = "GT",
+  designation = "Designation",
+  response = "Response",
+  pev,
+  lod,
+  fa,
+  eff,
+  maf
+) {
   moduleServer(id, function(input, output, session){
-    
     output$pevValue <- renderText(glue::glue("{round(pev,1)}%"))
     output$lodValue <- renderText(glue::glue("{round(lod,1)}"))
     output$faValue <- renderText(glue::glue("{fa}"))
     output$effValue <- renderText(glue::glue("{round(eff,3)}"))
     output$mafValue <- renderText(glue::glue("{round(maf*100,2)}%"))
 
-    
     pre_process_data <- reactive({
-      
       clean <- qtlData %>% 
         filter(!is.na(!!sym(response))) %>% 
         filter(!is.na(!!sym(designation)))
-      
-      
     })
-    
     output$allelicEffectBoxplot <- renderPlotly({
       req(pre_process_data)
       
@@ -69,8 +62,6 @@ allelicEffectPlot_server <- function(id, qtlData, gtCol = 'GT',
       traces <- lapply(Gcalls, function(GC){
         pre_process_data() %>%  filter(!!sym(gtCol) == GC) %>% pull(!!sym(response))
       })
-      
-      
       text <- lapply(Gcalls, function(GC){
         pre_process_data() %>%  filter(!!sym(gtCol) == GC) %>% pull(!!sym(designation))
       })
@@ -82,7 +73,7 @@ allelicEffectPlot_server <- function(id, qtlData, gtCol = 'GT',
         fig <- fig %>%
           add_trace(
             y = traces[[gt]],
-            type = 'box',
+            type = "box",
             name = gt,
             boxpoints = "all", jitter = 0.3,
             pointpos = -1.8,
@@ -91,40 +82,33 @@ allelicEffectPlot_server <- function(id, qtlData, gtCol = 'GT',
       }
       fig
     })
-    
   })
 }
 
-allelicEffect_demo <- function(){
+allelicEffect_demo <- function() {
   # Marker Info sheet
-  DB = 'data/FFAR_selections_SNP.SilicoDArT_4.xlsx'
-  df <- read_excel(path = DB, sheet = 'Marker info') %>% 
-    filter(AlleleID == '117740086-60-G/A') %>% 
+  DB = "/home/scruz/projects/EnvGwas_viewer/data/FFAR_selections_SNP.SilicoDArT_4.xlsx"
+  df <- read_excel(path = DB, sheet = "Marker info") %>% 
+    filter(AlleleID == "117740086-60-G/A") %>% 
     filter(variable == "Total Precipitation (mm)")
-  
   favorable_allele <- df$Favorable.allele
   allelic_effect <- df$effect
   maf <- df$maf2
   pev <- df$`Phenotype_Variance_Explained(%)`
   LOD <- -log10(df$`BLINK P value (Boferroni adjusted)`)
-  
   # Genotype  & Phenotype data
-  gdf <- read_excel(path = DB, sheet = 'Sheet2')
-  marker_id <- '117740086-60-G/A'
-  trait_id <- 'prec'
-  designation <- 'Accesion'
+  gdf <- read_excel(path = DB, sheet = "Sheet2")
+  marker_id <- "117740086-60-G/A"
+  trait_id <- "prec"
+  designation <- "Accesion"
   marker_name <- colnames(gdf)[grep(glue::glue("{marker_id}"), colnames(gdf))]
   tgt <- gdf[,c(designation, marker_name, trait_id)]
-  
-  
-  
   # TODO Filter one association
-  
   ui <- fluidPage(allelicEffectPlot_ui("x"))
   server <- function(input, output, session) {
-    allelicEffectPlot_server("x",tgt, gtCol = 'SNP_117740086-60-G/A_F_G',
-                             designation = 'Accesion',
-                             response = 'prec',
+    allelicEffectPlot_server("x",tgt, gtCol = "SNP_117740086-60-G/A_F_G",
+                             designation = "Accesion",
+                             response = "prec",
                              pev,
                              LOD,
                              favorable_allele,
--- app.R
+++ app.R
@@ -8,19 +8,84 @@ library(plotly)
 ui <- page_sidebar(
   title = "EnvGWAS V1.0",
   sidebar = sidebar(
-    title = "Test title"
+    title = "Control Panel",
+    selectInput(
+      inputId = "select_dataset",
+      label = "Select a dataset",
+      choices = NULL
+    ),
+    selectInput(
+      inputId = "select_genepool",
+      label = "Select a Genepool",
+      choices = c("Mesoamerican", "Andean")
+    )
   ),
-  allelicEffectPlot_ui('x')
+  uiOutput("main_area")
 )
 
-server <- function(input, output, session){
+server <- function(input, output, session) {
+  data_dir <- "data/"
+
+  observe({
+    files <- list.files(path = data_dir, pattern = "\\.xlsx$", full.names = FALSE)
+    updateSelectInput(session, "select_dataset", choices = files)
+  })
+
   read_data <- reactive({
-    DB = 'data/FFAR_selections_SNP.SilicoDArT_4.xlsx'
-    df <- read_excel(path = DB, sheet = 'Marker info')
+    req(input$select_dataset)
+    req(input$select_genepool)
+    file <- glue::glue("{data_dir}/{input$select_dataset}")
+    df <- read_excel(path = file, sheet = "Marker info") %>%
+      filter(Subset == input$select_genepool)
     df
   })
-  
-  allelicEffectPlot_server('x', read_data())
+  read_gt_data <- reactive({
+    req(read_data())
+    req(input$select_genepool)
+    file <- glue::glue("{data_dir}/{input$select_dataset}")
+    df <- read_excel(path = file, sheet = input$select_genepool)
+    df
+  })
+
+  output$main_area <- renderUI({
+    req(read_data())
+    trait_group <- read_data() %>%
+      group_by(variable) %>%
+      summarize(count = n())
+
+    gt_df <- read_gt_data()
+    tabs <- lapply(trait_group$variable, function(x) {
+      target_qtls <- read_data() %>%
+        filter(variable == x)
+      target_gt <- read_gt_data()
+      i_data <- lapply(target_qtls$qtn_id, function(y) {
+        i_metadata <- target_qtls %>%
+          filter(qtn_id == y)
+        i_gt <- target_gt %>%
+          select(Accesion, !!sym(x), !!sym(y))
+        list(
+          pev = i_metadata$`Phenotype_Variance_Explained(%)`,
+          lod = -log10(i_metadata$`BLINK P value (Boferroni adjusted)`),
+          fa = i_metadata$Favorable.allele,
+          eff = i_metadata$effect,
+          maf = i_metadata$maf2,
+          gt = i_gt
+        )
+      })
+
+      nav_panel(
+        x,
+        layout_column_wrap()
+      )
+    })
+
+
+    navset_card_tab(
+      title = "Traits",
+      !!!tabs
+    )
+  })
 }
 
 shinyApp(ui, server)
+
