f68b948efb92b5f604f5253158fd8d6f6325a0f2
serbier <serbier@users.noreply.github.com>
2025-04-30 16:19:48 -0500

    Add test data_model.R

--- R/data_model.R
+++ R/data_model.R
@@ -23,3 +23,27 @@ get_qtl_info <- function(m_info_df, m_id,id_col = "AlleleID"){
   m_info <- as.list(m_df[1,])
   return(m_info)
 }
+
+get_qtl_sample_data <- function(m_gt_df, m_id){
+  if(!is.data.frame(m_gt_df)) {
+    cli::cli_abort(glue::glue("m_gt_df is not a dataframe is {class(m_gt_df)}"))
+  } 
+  
+  if(!m_id %in% colnames(m_gt_df)) {
+    cli::cli_warn(glue::glue("{md_col} not in m_gt_df"))
+  }
+  
+  fmt_gt <- m_gt_df %>% 
+    mutate(GT = ifelse(!!sym(m_id) == !!sym(paste0(m_id, "_Imp")),
+                       !!sym(m_id), paste0(!!sym(paste0(m_id,"_Imp")),"_Imp")
+                       ))
+  return(fmt_gt)
+}
+
+DB = "data/FFAR_selections_SNP.SilicoDArT_4_v2.xlsx"
+m_info_df <- read_excel(path = DB, sheet = "Marker.info")
+marker_id <- "SNP_118390666-47-T/C"
+m_info <- get_qtl_info(m_info_df, marker_id)
+
+m_gt_df <- read_excel(path = DB, sheet = "Andean")
+gt_data <- get_qtl_sample_data(m_gt_df, marker_id)
